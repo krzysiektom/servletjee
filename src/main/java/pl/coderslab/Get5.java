b9e9eb04eb12509a6d25c72d04e18e12bfed043f
Krzysztof Tomala <krzysiektom@op.pl>
2018-12-27 10:26:31 +0100

    Get 5

--- src/main/java/pl/coderslab/Get5.java
+++ src/main/java/pl/coderslab/Get5.java
@@ -10,6 +10,22 @@ import java.io.IOException;
 @WebServlet("/Get5")
 public class Get5 extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html; charset=utf-8");
+        String company = request.getParameter("company");
+        String[] learn = request.getParameterValues("learn");
+        response.getWriter().append("company:")
+                .append("<br>");
+        response.getWriter().append("&nbsp&nbsp-&nbsp")
+                .append(company)
+                .append("<br>");
+        response.getWriter().append("learn:")
+                .append("<br>");
+        for (String s: learn) {
+            response.getWriter().append("&nbsp&nbsp-&nbsp")
+                    .append(s)
+                    .append("<br>");
+        }
+
 
     }
 }
