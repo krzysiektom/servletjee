4b2cd08a4cd067e1aa0c551e8fff18452329a977
Krzysztof Tomala <krzysiektom@op.pl>
2018-12-27 09:58:07 +0100

    Get 3

---
+++ src/main/java/pl/coderslab/Get2.java
@@ -0,0 +1,26 @@
+package pl.coderslab;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+
+@WebServlet("/Get2")
+public class Get2 extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html; charset=utf-8");
+        Map<String, String[]> paramMap = request.getParameterMap();
+
+        for (String key : paramMap.keySet()) {
+            response.getWriter().append(key)
+                    .append(": ")
+                    .append(Arrays.toString(paramMap.get(key)))
+                    .append("<br>");
+
+        }
+    }
+}
